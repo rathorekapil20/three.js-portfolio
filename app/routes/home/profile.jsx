e6955b2572d6223e8578d7959b750d1b06644ea1
Kapil Rathore <116137491+rathorekapil20@users.noreply.github.com>
2025-01-22 00:24:15 +0530

    init

---
+++ app/routes/home/profile.jsx
@@ -0,0 +1,104 @@
+import profileImgLarge from '~/assets/profile-large.jpg';
+import profileImgPlaceholder from '~/assets/profile-placeholder.jpg';
+import profileImg from '~/assets/profile.jpg';
+import { Button } from '~/components/button';
+import { DecoderText } from '~/components/decoder-text';
+import { Divider } from '~/components/divider';
+import { Heading } from '~/components/heading';
+import { Image } from '~/components/image';
+import { Link } from '~/components/link';
+import { Section } from '~/components/section';
+import { Text } from '~/components/text';
+import { Transition } from '~/components/transition';
+import { Fragment, useState } from 'react';
+import { media } from '~/utils/style';
+import katakana from './katakana.svg';
+import styles from './profile.module.css';
+
+const ProfileText = ({ visible, titleId }) => (
+  <Fragment>
+    <Heading className={styles.title} data-visible={visible} level={3} id={titleId}>
+      <DecoderText text="Hi there" start={visible} delay={500} />
+    </Heading>
+    <Text className={styles.description} data-visible={visible} size="l" as="p">
+      I’m Kapil, currently living in Indore and working as a full-stack developer at{' '}
+      <Link href="https://www.rentprompts.com">RentPrompts</Link>. My work involves building
+      interactive and responsive web applications using technologies like React, 
+      TypeScript, Next.js and MongoDB. Being comfortable with both frontend and 
+      backend development allows me to deliver fast and modern web solutions efficiently. 
+      If you’re interested in the tools and software I use, check out my{' '}
+      <Link href="/uses">uses page</Link>.
+    </Text>
+    <Text className={styles.description} data-visible={visible} size="l" as="p">
+      In my spare time, I enjoy exploring new tech trends, playing video games, and working
+      on side projects. I’m always open to hearing about exciting new opportunities, so feel 
+      free to drop me a line.
+    </Text>
+</Fragment>
+
+);
+
+export const Profile = ({ id, visible, sectionRef }) => {
+  const [focused, setFocused] = useState(false);
+  const titleId = `${id}-title`;
+
+  return (
+    <Section
+      className={styles.profile}
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      as="section"
+      id={id}
+      ref={sectionRef}
+      aria-labelledby={titleId}
+      tabIndex={-1}
+    >
+      <Transition in={visible || focused} timeout={0}>
+        {({ visible, nodeRef }) => (
+          <div className={styles.content} ref={nodeRef}>
+            <div className={styles.column}>
+              <ProfileText visible={visible} titleId={titleId} />
+              <Button
+                secondary
+                className={styles.button}
+                data-visible={visible}
+                href="/contact"
+                icon="send"
+              >
+                Send me a message
+              </Button>
+            </div>
+            <div className={styles.column}>
+              <div className={styles.tag} aria-hidden>
+                <Divider
+                  notchWidth="64px"
+                  notchHeight="8px"
+                  collapsed={!visible}
+                  collapseDelay={1000}
+                />
+                <div className={styles.tagText} data-visible={visible}>
+                  About me
+                </div>
+              </div>
+              <div className={styles.image}>
+                <Image
+                  reveal
+                  delay={100}
+                  placeholder={profileImgPlaceholder}
+                  srcSet={`${profileImg} 480w, ${profileImgLarge} 960w`}
+                  width={960}
+                  height={1280}
+                  sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
+                  alt="Me smiling like a goofball at the RentPrompts office in Indore"
+                />
+                {/* <svg className={styles.svg} data-visible={visible} viewBox="0 0 136 766">
+                  <use href={`${katakana}#katakana-profile`} />
+                </svg> */}
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </Section>
+  );
+};
